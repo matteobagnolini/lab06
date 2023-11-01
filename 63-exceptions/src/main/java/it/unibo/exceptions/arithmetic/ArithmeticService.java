a7dc06e0974bda85488eee4c769594ee9e92ae10
Matteo Bagnolini <matteobagnolini2003@gmail.com>
2023-11-01 16:06:07 +0100

    resolved bug in ArithmeticService.java.

--- 63-exceptions/src/main/java/it/unibo/exceptions/arithmetic/ArithmeticService.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/arithmetic/ArithmeticService.java
@@ -100,18 +100,18 @@ public final class ArithmeticService {
     
     private void computeAt(final int operatorIndex) {
         if (operatorIndex == 0) {
-            System.out.println("Illegal start of operation: " + commandQueue);
+            throw new IllegalStateException("Illegal start of operation: " + commandQueue);
         }
         if (commandQueue.size() < 3) {
-            System.out.println("Not enough operands: " + commandQueue);
+            throw new IllegalStateException("Not enough operands: " + commandQueue);
         }
         if (commandQueue.size() < operatorIndex + 1) {
-            System.out.println("Missing right operand: " + commandQueue);
+            throw new IllegalStateException("Missing right operand: " + commandQueue);
         }
         final var rightOperand = commandQueue.remove(operatorIndex + 1);
         final var leftOperand = commandQueue.remove(operatorIndex - 1);
         if (KEYWORDS.contains(rightOperand) || KEYWORDS.contains(leftOperand)) {
-            System.out.println(
+            throw new IllegalStateException(
                 "Expected a number, but got " + leftOperand + " and " + rightOperand + " in " + commandQueue
             );
         }
@@ -123,10 +123,7 @@ public final class ArithmeticService {
             case MINUS -> left - right;
             case TIMES -> left * right;
             case DIVIDED -> left / right;
-            default ->  {
-                System.out.println("Unknown operand " + operand);
-                yield Double.NaN;
-            }
+            default ->  throw new IllegalStateException("Unknown operand " + operand);
         };
         commandQueue.set(operatorIndex - 1, Double.toString(result));
     }
