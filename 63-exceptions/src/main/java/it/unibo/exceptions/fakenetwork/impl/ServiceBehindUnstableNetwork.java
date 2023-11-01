077ef4639d43987af3ea9472637d3d5a0da5bca5
Matteo Bagnolini <matteobagnolini2003@gmail.com>
2023-11-01 12:26:10 +0100

    excercise 1 of 63-exceptions
    'excercise 1 of 6.3'

--- 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
@@ -29,6 +29,10 @@ public final class ServiceBehindUnstableNetwork implements NetworkComponent {
         /*
          * The probability should be in [0, 1[!
          */
+        if (failProbability < 0 || failProbability >= 1) {
+            String msg = new String ("probabilità deve essere compresa tra 0 e 1");
+            throw new IllegalArgumentException(msg);
+        }
         this.failProbability = failProbability;
         randomGenerator = new Random(randomSeed);
     }
