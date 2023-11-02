601fa744aa5d6fd7e62d7122c26c39c274fbfe9f
Matteo <matteobagnolini2003@gmail.com>
2023-11-02 10:59:12 +0100

    impl Graph except last method

---
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
@@ -0,0 +1,50 @@
+package it.unibo.generics.graph.impl;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import it.unibo.generics.graph.api.Graph;
+
+public class GraphImpl implements Graph<String> {
+
+    Map<String, Set<String>> graph = new HashMap<>();
+    
+    public GraphImpl () {
+    }
+
+
+    @Override
+    public void addNode(String node) {
+        if (!this.graph.containsKey(node)) {
+            Set<String> connectedTo = new HashSet<>();
+            this.graph.put(node, connectedTo);
+        }
+    }
+
+    @Override
+    public void addEdge(String source, String target) {
+        if (this.graph.containsKey(source)) {
+            this.graph.get(source).add(target);
+        }
+    }
+
+    @Override
+    public Set<String> nodeSet() {
+        return this.graph.keySet();
+    }
+
+    @Override
+    public Set<String> linkedNodes(String node) {
+        return this.graph.get(node);
+    }
+
+    @Override
+    public List<String> getPath(String source, String target) {
+
+        return null;
+    }
+    
+}
