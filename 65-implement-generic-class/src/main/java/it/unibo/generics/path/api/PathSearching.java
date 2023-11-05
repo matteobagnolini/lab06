73e49e72f6261ed946ea8bb7eef820681b4f8e34
Matteo Bagnolini <matteobagnolini2003@gmail.com>
2023-11-05 20:43:38 +0100

    added new interface and created new class PathSearcing

---
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/path/api/PathSearching.java
@@ -0,0 +1,9 @@
+package it.unibo.generics.path.api;
+
+import java.util.List;
+
+public interface PathSearching <N> {
+    
+    List<N> searchingAlgortithm ();
+
+}
---
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/path/impl/FrontierAlgorithm.java
@@ -0,0 +1,15 @@
+package it.unibo.generics.path.impl;
+
+import java.util.List;
+
+import it.unibo.generics.path.api.PathSearching;
+
+public class FrontierAlgorithm<N> implements PathSearching {
+
+    @Override
+    public List<N> searchingAlgortithm() {
+
+        return null;
+    }
+
+}
